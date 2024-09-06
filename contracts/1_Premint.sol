4bbbf290972312d7bb90b1bbd1d68736be126b84
Nambi <harrisonlvl2@gmail.com>
2024-09-06 13:15:33 +0530

    smart contract changes

--- contracts/1_Premint.sol
+++ contracts/1_Premint.sol
@@ -5,16 +5,18 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract Premint {
 
-    mapping (address => bool) applicants;
+    mapping (uint => address) applicants;
+    mapping (address => bool) applied;
     uint count = 0;
     uint collected_total = 0;
     
     // apply for an early adopter pass
     function apply_for_pass() public payable {
-        require (applicants[msg.sender] == false, "you have already applied");
+        require (applied[msg.sender] == false, "you have already applied");
         require (msg.value >= 0.01 ether, "the deposit must be at least 0.01 ether");
-        require (count <= 1000);
-        applicants[msg.sender] = true;
+        require (count < 1000);
+        applied[msg.sender] = true;
+        applicants[count + 1] = msg.sender;
         count = count + 1;
         collected_total = collected_total + msg.value; 
     }
